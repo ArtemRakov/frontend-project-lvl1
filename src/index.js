a1b97202cacc0d48ba585c54f35479eb9da72c68
Artem Rakov <scoff1k@gmail.com>
2019-08-01 16:45:24 +0300

    Step 3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import readlineSync from 'readline-sync';
 
 const run = () => {
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`)
-}
+  console.log(`Hello, ${name}!`);
+};
 
 export default run;
-
