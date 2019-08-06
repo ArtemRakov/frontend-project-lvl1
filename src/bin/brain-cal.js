2191937880b72a072756bfca301ea09a11cd1655
Artem Rakov <scoff1k@gmail.com>
2019-08-06 14:56:50 +0300

    game without classes

--- src/bin/brain-cal.js
+++ src/bin/brain-cal.js
@@ -1,18 +1,8 @@
 #!/usr/bin/env node
 
-import Game from '../index';
+import run from '..';
 import validation from '../games/brain-cal/validation';
 import question from '../games/brain-cal/question';
 import instruction from '../games/brain-cal/instruction';
 
-class BrainCal extends Game {
-  constructor(name) {
-    super(name);
-    this.validation = validation;
-    this.question = question;
-    this.instruction = instruction;
-  }
-}
-
-
-BrainCal.run();
+run(instruction, question, validation);
--- src/bin/brain-gcd.js
+++ src/bin/brain-gcd.js
@@ -1,19 +1,8 @@
 #!/usr/bin/env node
 
-import Game from '../index';
+import run from '..';
 import validation from '../games/brain-gcd/validation';
 import question from '../games/brain-gcd/question';
 import instruction from '../games/brain-gcd/instruction';
 
-
-class BrainGcd extends Game {
-  constructor(name) {
-    super(name);
-    this.validation = validation;
-    this.question = question;
-    this.instruction = instruction;
-  }
-}
-
-
-BrainGcd.run();
+run(instruction, question, validation);
