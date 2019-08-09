61339cbb52a61fd5f4647c15e6512e6ac9e57a88
Artem Rakov <scoff1k@gmail.com>
2019-08-09 16:41:16 +0300

    refactored cal

--- src/games/cal.js
+++ src/games/cal.js
@@ -7,16 +7,16 @@ const operations = [
   { sign: '*', solve: (a, b) => a * b },
 ];
 
-const instruction = () => console.log('What is the result of the expression? \n');
+const instruction = () => 'What is the result of the expression? \n';
 
-const validation = result => String(result);
 
-const question = () => {
+const data = () => {
   const number1 = randomize(1, 25);
   const number2 = randomize(1, 25);
   const operation = operations[randomize(0, operations.length - 1)];
-  console.log(`${number1} ${operation.sign} ${number2}`);
-  return operation.solve(number1, number2);
+  const question = (`${number1} ${operation.sign} ${number2}`);
+  const correctAnswer = operation.solve(number1, number2).toString();
+  return { question, correctAnswer };
 };
 
-export default () => run(instruction, question, validation);
+export default () => run(data, instruction);
