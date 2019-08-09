ac09c11a135bdc0645ffd64019a68d4e137752e2
Artem Rakov <scoff1k@gmail.com>
2019-08-09 16:53:08 +0300

    refactor prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,7 @@
 import randomize from '../randomize';
 import run from '..';
 
-const instruction = () => console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'. \n");
+const instruction = () => "Answer 'yes' if given number is prime. Otherwise answer 'no'. \n";
 
 const isPrime = (number) => {
   for (let i = 2, s = Math.sqrt(number); i <= s; i += 1) {
@@ -11,12 +11,12 @@ const isPrime = (number) => {
   return number > 1;
 };
 
-const question = () => {
-  const number = randomize(1, 30);
-  console.log(`Question: ${number}`);
-  return number;
+
+const data = () => {
+  const question = randomize(1, 30);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return { question, correctAnswer };
 };
 
-const validation = number => (isPrime(number) ? 'yes' : 'no');
-
-export default () => run(instruction, question, validation);
+export default () => run(data, instruction);
